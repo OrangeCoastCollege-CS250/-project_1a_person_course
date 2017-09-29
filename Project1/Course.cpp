06e864e7908ff7e667b297601d9b6f70a4c18a9a
Severiano Badajoz <sevebadajoz@gmail.com>
2017-09-29 12:03:59 -0700

    Implement Course functions

--- Project1/Course.cpp
+++ Project1/Course.cpp
@@ -1,29 +1,55 @@
 /*
-	(name header)
+Badajoz, Severiano
+Vu, Alexis
+
+CS A150
+September 29,2017
+
+Project 1(Part A)
 */
 
 #include "Course.h"
 
 // default constructor
-
+Course::Course() {
+	courseCredits = 0;
+	courseGrade = '*';
+}
 
 // setCourseInfo
-
+void Course::setCourseInfo(const string& name, const string& number, const char grade, const int credits) {
+	courseName = name;
+	courseNo = number;
+	courseGrade = grade;
+	courseCredits = credits;
+}
 
 // printCourseInfo
+void Course::printCourseInfo(const bool printable) const {
+	cout << setw(15);
+	cout << courseNo << courseName << courseCredits << courseGrade << endl;
+}
 
-
-// getCourseCredits
-
+/// getCourseCredits
+int Course::getCourseCredits() const {
+	return courseCredits;
+}
 
 // getCourseNumber
-
+string Course::getCourseNumber() const {
+	return courseNo;
+}
 
 // getCourseName
-
+string Course::getCourseName() const {
+	return courseName;
+}
 
 // getCourseGrade
+char Course::getCourseGrade() const {
+	return courseGrade;
+}
 
 
 // destructor
-
+Course::~Course() {}
